03dca6c01b190df151409c4a1f1fbd53efad7396
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2025-09-01 15:23:28 -0300

    construcao doe controller, application.yml, dependencias e outros

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -0,0 +1,47 @@
+package com.algaworks.algasensors.temperature.processing.api.controller;
+
+import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
+import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/temperatures/data")
+@Slf4j
+public class TemperatureProcessingController {
+
+    // É void pois não precisaremos responder nada de volta pro sensor
+    @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
+    public void data(@PathVariable TSID sensorId, @RequestBody String input) {
+
+        if (input == null || input.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // Lendo os dados da temperatura que são enviados no sensor
+        Double temperature;
+
+        try {
+            temperature = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        TemperatureLogOutput logOutput = TemperatureLogOutput.builder()
+                .id(IdGenerator.generateTimeBasedUUID())
+                .sensorId(sensorId)
+                .value(temperature)
+                .registeredAt(OffsetDateTime.now())
+                .build();
+
+        log.info(logOutput.toString());
+
+    }
+
+}
